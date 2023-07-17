cdfd7a9c1b1c32f5cead19a5314a9b5e6164f77c
Khushi Singh <76054119+Khushi5-cloud@users.noreply.github.com>
2023-07-17 15:32:51 +0530

    Add files via upload

---
+++ QR Code Based Attendance System/qr_gen.py
@@ -0,0 +1,29 @@
+import openpyxl
+import qrcode
+
+wb=openpyxl.load_workbook('Student_names.xlsx')
+s=wb.active
+
+l=[]
+for i in s.iter_cols(min_row=1,min_col=1,max_row=5,max_col=1):
+    for c in i:
+        ch=c.value.split("\n")
+        l.append(ch[0])
+        # hhh
+        # hhh
+        # jjjj
+        # ddd
+        # hhh
+        #img=qrcode.make(ch)
+        #img.save("img.png")
+#print(len(l))
+for i in range(0,len(l)):
+    img=qrcode.make(l[i])
+    img.save("img %s.png"%(i+1)) # formatting the name of img
+    
+
+    
+
+
+
+
